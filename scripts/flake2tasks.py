90416f400234c88c136632441edf0dd930cbe905
HapiFive <1143910315@qq.com>
2024-04-03 04:55:36 +0800

    添加脚本

---
+++ scripts/flake2tasks.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Copyright (C) 2019 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+'''
+flake2tasks.py - Convert flake8 warnings into Qt Creator task files.
+
+SYNOPSIS
+
+    flake2tasks.py < logfile > taskfile
+'''
+
+import sys
+import re
+
+if __name__ == '__main__':
+    pattern = re.compile(r'^([^:]+):(\d+):\d+: (.*)$')
+    n = 0
+    while True:
+        line = sys.stdin.readline().rstrip()
+        if not line:
+            break
+        match = pattern.match(line)
+        if match:
+            file_name = match.group(1).replace('\\', '/')
+            line_number = match.group(2)
+            text = match.group(3)
+            print(f"{file_name}\t{line_number}\twarn\t{text}")
+            n += 1
+    if n:
+        print(f"{n} issue(s) found.", file=sys.stderr)
---
+++ scripts/makedmg.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# Copyright (C) 2018 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+import argparse
+import os
+import shutil
+import subprocess
+import tempfile
+import time
+
+import common
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Create Qt Creator disk image, filtering out debug information files.',
+        epilog="To sign the contents before packaging on macOS, set the SIGNING_IDENTITY and optionally the SIGNING_FLAGS environment variables.")
+    parser.add_argument('target_diskimage', help='output .dmg file to create')
+    parser.add_argument('dmg_volumename', help='volume name to use for the disk image')
+    parser.add_argument('source_directory', help='directory with the Qt Creator sources')
+    parser.add_argument('binary_directory', help='directory that contains the Qt Creator.app')
+    parser.add_argument('--dmg-size', default='1500m', required=False)
+    parser.add_argument('--license-replacement', default=None,
+        help='Absolute path to a license file which replaces the default LICENSE.GPL3-EXCEPT from Qt Creator source directory.')
+    parser.add_argument('--keep-signed-content-at', default=None,
+        help='For online installer we want to keep the signed .app without the dmg. This is used to create a 7z.')
+    return parser.parse_args()
+
+def main():
+    arguments = parse_arguments()
+    tempdir_base = tempfile.mkdtemp()
+    tempdir = os.path.join(tempdir_base, os.path.basename(arguments.binary_directory))
+    try:
+        common.copytree(arguments.binary_directory, tempdir, symlinks=True, ignore=common.is_debug)
+        if common.is_mac_platform():
+            app_path = [app for app in os.listdir(tempdir) if app.endswith('.app')][0]
+            common.codesign(os.path.join(tempdir, app_path))
+        os.symlink('/Applications', os.path.join(tempdir, 'Applications'))
+        license_file = os.path.join(arguments.source_directory, 'LICENSE.GPL3-EXCEPT')
+        if (arguments.license_replacement):
+            license_file = arguments.license_replacement
+        shutil.copy(license_file, tempdir)
+        dmg_cmd = ['hdiutil', 'create', '-srcfolder', tempdir, '-volname', arguments.dmg_volumename,
+                   '-format', 'ULMO', arguments.target_diskimage, '-ov', '-scrub', '-size', arguments.dmg_size, '-verbose']
+        subprocess.check_call(dmg_cmd)
+        # sleep a few seconds to make sure disk image is fully unmounted etc
+        time.sleep(5)
+    finally:
+        if arguments.keep_signed_content_at:
+            shutil.move(tempdir, arguments.keep_signed_content_at)
+        else:
+            shutil.rmtree(tempdir_base)
+if __name__ == "__main__":
+    main()
