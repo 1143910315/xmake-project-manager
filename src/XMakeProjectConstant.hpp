d74d806ae5051f4a7a5d8c7a8abae93243a90e96
HapiFive <1143910315@qq.com>
2024-04-03 14:11:29 +0800

    auto个屁，全端了！

--- src/XMakeProjectConstant.hpp
+++ src/XMakeProjectConstant.hpp
@@ -8,41 +8,41 @@
 
 namespace XMakeProjectManager::Constants {
     namespace Icons {
-        static constexpr auto XMAKE              = ":/xmakeproject/icons/xmake_logo.png";
-        static constexpr auto XMAKE_FILE_OVERLAY = ":/xmakeproject/icons/xmake_file_overlay.png";
-        static constexpr auto XMAKE_BW           = ":/xmakeproject/icons/xmake_bw_logo.png";
+        static constexpr const char * XMAKE              = ":/xmakeproject/icons/xmake_logo.png";
+        static constexpr const char * XMAKE_FILE_OVERLAY = ":/xmakeproject/icons/xmake_file_overlay.png";
+        static constexpr const char * XMAKE_BW           = ":/xmakeproject/icons/xmake_bw_logo.png";
     } // namespace Icons
 
     namespace Project {
-        static constexpr auto MIMETYPE = "text/x-xmake-project";
-        static constexpr auto ID       = "XMakeProjectManager.XMakeProject";
+        static constexpr const char * MIMETYPE = "text/x-xmake-project";
+        static constexpr const char * ID       = "XMakeProjectManager.XMakeProject";
     } // namespace Project
 
     namespace SettingsPage {
-        static constexpr auto GENERAL_ID = "A.XMakeProjectManager.SettingsPage.General";
-        static constexpr auto TOOLS_ID   = "Z.XMakeProjectManager.SettingsPage.Tools";
-        static constexpr auto CATEGORY   = "Z.XMake";
+        static constexpr const char * GENERAL_ID = "A.XMakeProjectManager.SettingsPage.General";
+        static constexpr const char * TOOLS_ID   = "Z.XMakeProjectManager.SettingsPage.Tools";
+        static constexpr const char * CATEGORY   = "Z.XMake";
     } // namespace SettingsPage
 
     namespace ToolsSettings {
-        static constexpr auto FILENAME         = "xmaketool.xml";
-        static constexpr auto ENTRY_KEY        = "XMakeProjectManager.Tool.";
-        static constexpr auto ENTRY_COUNT      = "XMakeProjectManager.Tools.Count";
-        static constexpr auto EXE_KEY          = "XMakeProjectManager.Tool.Exe";
-        static constexpr auto AUTODETECTED_KEY = "XMakeProjectManager.Tool.AutoDetected";
-        static constexpr auto NAME_KEY         = "XMakeProjectManager.Tool.name";
-        static constexpr auto UUID_KEY         = "XMakeProjectManager.Tool.uuid";
-        static constexpr auto TOOL_TYPE_XMAKE  = "xmake";
-        static constexpr auto TOOL_TYPE_XREPO  = "xrepo"; /* for the future */
-        static constexpr auto TOOL_TYPE_KEY    = "XMakeProjectManager.Tool.Type";
-        static constexpr auto AUTORUN_KEY      = "XMakeProjectManager.Tool.Autorun";
-        static constexpr auto AUTO_ACCEPT_REQUESTS_KEY =
+        static constexpr const char * FILENAME         = "xmaketool.xml";
+        static constexpr const char * ENTRY_KEY        = "XMakeProjectManager.Tool.";
+        static constexpr const char * ENTRY_COUNT      = "XMakeProjectManager.Tools.Count";
+        static constexpr const char * EXE_KEY          = "XMakeProjectManager.Tool.Exe";
+        static constexpr const char * AUTODETECTED_KEY = "XMakeProjectManager.Tool.AutoDetected";
+        static constexpr const char * NAME_KEY         = "XMakeProjectManager.Tool.name";
+        static constexpr const char * UUID_KEY         = "XMakeProjectManager.Tool.uuid";
+        static constexpr const char * TOOL_TYPE_XMAKE  = "xmake";
+        static constexpr const char * TOOL_TYPE_XREPO  = "xrepo"; /* for the future */
+        static constexpr const char * TOOL_TYPE_KEY    = "XMakeProjectManager.Tool.Type";
+        static constexpr const char * AUTORUN_KEY      = "XMakeProjectManager.Tool.Autorun";
+        static constexpr const char * AUTO_ACCEPT_REQUESTS_KEY =
             "XMakeProjectManager.Tool.AutoAcceptRequests";
     } // namespace ToolsSettings
 
     namespace BuildConfiguration {
-        static constexpr auto BUILD_TYPE_KEY = "XMakeProjectManager.BuildConfig.Type";
-        static constexpr auto PARAMETERS_KEY = "XMakeProjectManager.BuildConfig.Parameters";
+        static constexpr const char * BUILD_TYPE_KEY = "XMakeProjectManager.BuildConfig.Type";
+        static constexpr const char * PARAMETERS_KEY = "XMakeProjectManager.BuildConfig.Parameters";
     } // namespace BuildConfiguration
 
     namespace BuildStep {
@@ -52,36 +52,36 @@ namespace XMakeProjectManager::Constants {
     } // namespace BuildStep
 
     namespace Targets {
-        static constexpr auto ALL   = "all";
-        static constexpr auto CLEAN = "clean";
+        static constexpr const char * ALL   = "all";
+        static constexpr const char * CLEAN = "clean";
     } // namespace Targets
 
-    static constexpr auto XMAKE_INFO_DIR = ".xmake";
+    static constexpr const char * XMAKE_INFO_DIR = ".xmake";
 
 #if defined(Q_OS_WINDOWS)
     #if INTPTR_MAX == INT64_MAX
-    static constexpr auto XMAKE_INTRO_CONF = "/windows/x64/xmake.conf";
+    static constexpr const char * XMAKE_INTRO_CONF = "/windows/x64/xmake.conf";
     #else
-    static constexpr auto XMAKE_INTRO_CONF = "/windows/x32/xmake.conf";
+    static constexpr const char * XMAKE_INTRO_CONF = "/windows/x32/xmake.conf";
     #endif
 #elif defined(Q_OS_LINUX)
     #if INTPTR_MAX == INT64_MAX
-    static constexpr auto XMAKE_INTRO_CONF = "/linux/x64/xmake.conf";
+    static constexpr const char * XMAKE_INTRO_CONF = "/linux/x64/xmake.conf";
     #else
-    static constexpr auto XMAKE_INTRO_CONF = "/linux/x32/xmake.conf";
+    static constexpr const char * XMAKE_INTRO_CONF = "/linux/x32/xmake.conf";
     #endif
 #elif defined(Q_OS_MACOS)
     #if INTPTR_MAX == INT64_MAX
-    static constexpr auto XMAKE_INTRO_CONF = "/macos/x64/xmake.conf";
+    static constexpr const char * XMAKE_INTRO_CONF = "/macos/x64/xmake.conf";
     #else
-    static constexpr auto XMAKE_INTRO_CONF = "/macos/x32/xmake.conf";
+    static constexpr const char * XMAKE_INTRO_CONF = "/macos/x32/xmake.conf";
     #endif
 #endif
 
-    static constexpr auto TOOL_NAME = "xmake";
-    static constexpr auto TOOL_ID   = "XMakeProjectManager.XMakeKitInformation.XMake";
+    static constexpr const char * TOOL_NAME = "xmake";
+    static constexpr const char * TOOL_ID   = "XMakeProjectManager.XMakeKitInformation.XMake";
 
-    static constexpr auto XMAKE_TOOL_MANAGER    = "XMakeProjectManager.Tools";
-    static constexpr auto XMAKE_BUILD_STEP_ID   = "XMakeProjectManager.BuildStep";
-    static constexpr auto XMAKE_BUILD_CONFIG_ID = "XMakeProjectManager.BuildConfiguration";
+    static constexpr const char * XMAKE_TOOL_MANAGER    = "XMakeProjectManager.Tools";
+    static constexpr const char * XMAKE_BUILD_STEP_ID   = "XMakeProjectManager.BuildStep";
+    static constexpr const char * XMAKE_BUILD_CONFIG_ID = "XMakeProjectManager.BuildConfiguration";
 } // namespace XMakeProjectManager::Constants
